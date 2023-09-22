866bf9df64c378e9d3afbb34e7696cf25870ce5a
Elias Alejandro Diaz Cahuas <N00369765@upn.pe>
2023-09-21 23:58:46 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics.CodeAnalysis;
+
+Console.WriteLine("\tPrograma para hallar el promedio de 'n notas'");
+Console.WriteLine("\t·············································\n");
+Console.Write("Ingrese el número de notas: ");
+int n = int.Parse(Console.ReadLine());
+float suma = 0;
+
+for (int i = 1; i <= n; i++)
+{
+    Console.Write("Ingrese la nota " + i + ": ");
+    float nota = float.Parse(Console.ReadLine());
+    suma += nota;
+}
+suma /= n;
+
+Console.WriteLine("El promedio es: {0}", suma);
+Console.ReadKey();
